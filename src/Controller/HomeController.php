8a0d8ea374fe6ea322754d0bafcd6f2dbcfbfbdc
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-01-24 00:13:25 +0100

    MAIN > Création des descriptions des personnages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HttpClientInterface $httpClient): Response
     {
+        $personnages = $httpClient->request(
+            'GET',
+            'https://swapi.py4e.com/api/people/'
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'personnages' => $personnages->toArray()['results'],
         ]);
     }
 }
